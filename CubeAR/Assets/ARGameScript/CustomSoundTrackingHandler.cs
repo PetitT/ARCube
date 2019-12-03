7914f476032d2dfe466bc35b64e85315fffa9064
PetitT <xcthib@hotmail.com>
2019-12-03 09:28:57 +0100

    First Push

---
+++ CubeAR/Assets/ARGameScript/CustomSoundTrackingHandler.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CustomSoundTrackingHandler : DefaultTrackableEventHandler
+{
+    public AudioSource audio;
+
+    protected override void OnTrackingFound()
+    {
+        base.OnTrackingFound();
+        audio.Play();
+    }
+
+    protected override void OnTrackingLost()
+    {
+        base.OnTrackingLost();
+        audio.Pause();
+    }
+}
---
+++ CubeAR/Assets/ARGameScript/Switch.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Vuforia;
+
+public class Switch : MonoBehaviour, IVirtualButtonEventHandler
+{
+    public GameObject good;
+    public GameObject bad;
+    public int number = 0;
+    public int maxNumber = 2;
+    public VirtualButtonBehaviour vb;
+
+    private void Start()
+    {
+        vb.RegisterEventHandler(this);
+    }
+
+    public void OnButtonPressed(VirtualButtonBehaviour vb)
+    {
+        number++;
+        if (number >= maxNumber)
+        {
+            bad.SetActive(true);
+            good.SetActive(false);
+        }
+    }
+
+    public void OnButtonReleased(VirtualButtonBehaviour vb)
+    {
+        
+    }
+}
+
